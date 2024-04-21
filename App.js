b7184a64e6de16e0531c1565e33956373f86e5a8
Manpreet Singh <manpreetmss400@gmail.com>
2024-04-21 19:48:17 +0530

    hello world in react-native with expo

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: 'red' }}>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: 'red',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
